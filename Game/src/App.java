7e40d302c9afe153c59f2bddcc2e72976d4c52b1
g4bx <gabriel.samurai00@gmail.com>
2023-05-24 21:33:39 -0300

    Criando tela de login

---
+++ Game/src/App.java
@@ -0,0 +1,8 @@
+import view.LoginMenu;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Loging...");
+        new LoginMenu();        
+    }
+}
---
+++ Game/src/view/LoginMenu.java
@@ -0,0 +1,18 @@
+package view;
+
+import javax.swing.*;
+import javax.swing.JFrame;
+
+public class LoginMenu {
+    private JFrame telaLogin;
+
+    public LoginMenu(){
+        telaLogin = new JFrame("BEM-VINDO");
+        telaLogin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        telaLogin.setSize(400, 300);
+        telaLogin.setResizable(false);
+        telaLogin.setVisible(true);
+    }
+    
+}
