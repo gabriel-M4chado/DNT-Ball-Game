a8214df7f7d30ff1fb6fefd1c3105a87b1f4595d
g4bx <gabriel.machado.js@gmail.com>
2023-05-26 22:05:43 -0300

    Criando os campos de nome e email
    os componentes ficaram centralizados em tela

--- Game/src/view/TelaCadastro.java
+++ Game/src/view/TelaCadastro.java
@@ -1,6 +1,10 @@
 package view;
 
 import javax.swing.*;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
@@ -10,8 +14,13 @@ import java.awt.event.WindowEvent;
 public class TelaCadastro extends JFrame implements ActionListener{
 
     private JFrame telaCadastro;
+    private JPanel containerTela;
+    private JLabel lblNome;
+    private JTextField jtfNome;
+    private JLabel lblEmail;
+    private JTextField jtfEmail;
     
-    public TelaCadastro(){
+    public TelaCadastro (){
         this.telaCadastro = new JFrame("Cadastro");
 
         telaCadastro.addWindowListener(new WindowAdapter() {
@@ -21,11 +30,53 @@ public class TelaCadastro extends JFrame implements ActionListener{
             }
         });
 
+        containerTela = formCadastro();
+
+        telaCadastro.setContentPane(containerTela);
         telaCadastro.setSize(450, 600);
         telaCadastro.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
         telaCadastro.setResizable(false);
         telaCadastro.setVisible(true);
         telaCadastro.setLocationRelativeTo(null);
+    }
+
+    public JPanel formCadastro() {
+
+        lblNome = new JLabel("Nome:");
+        jtfNome = new JTextField(20);
+
+        lblEmail = new JLabel("E-mail:");
+        jtfEmail = new JTextField(20);
+
+        containerTela = new JPanel();
+        containerTela.setLayout(new GridBagLayout());
+
+        GridBagConstraints atributosGrid = new GridBagConstraints();
+        atributosGrid.anchor = GridBagConstraints.WEST;
+        atributosGrid.insets = new Insets(5, 5, 5, 5);
+
+        atributosGrid.gridx = 0;
+        atributosGrid.gridy = 0;
+        containerTela.add(lblNome, atributosGrid);
+
+        atributosGrid.gridx = 1;
+        atributosGrid.gridy = 0;
+        containerTela.add(jtfNome, atributosGrid);
+
+        atributosGrid.gridx = 0;
+        atributosGrid.gridy = 2;
+        containerTela.add(lblEmail, atributosGrid);
+
+        atributosGrid.gridx = 1;
+        atributosGrid.gridy = 2;
+        containerTela.add(jtfEmail, atributosGrid);
+
+        atributosGrid.gridx = 0;
+        atributosGrid.gridy = 3;
+        atributosGrid.anchor = GridBagConstraints.NORTH;
+        containerTela.add(Box.createVerticalGlue(), atributosGrid);
+
+        return containerTela;
     }
 
     @Override
