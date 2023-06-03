0b101be7b5f7bcf0143826e5f059dcff9698dd89
g4bx <gabriel.machado.js@gmail.com>
2023-06-03 11:15:46 -0300

    Adicionando a barra do jogo

--- Game/src/view/BallGame.java
+++ Game/src/view/BallGame.java
@@ -4,10 +4,16 @@ import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.JPanel;
 
-public class BallGame extends JPanel{
+public class BallGame extends JPanel {
     protected void paintComponent(Graphics ball) {
         super.paintComponent(ball);
         ball.setColor(Color.RED);
         ball.fillOval(150, 150, 50, 50);
+
+        // Barra do jogo 
+        ball.setColor(Color.BLACK);
+        int lineWidth = 200; 
+        int lineY = getHeight() - 20; 
+        ball.fillRect(150, lineY, lineWidth, 10);
     }
 }
