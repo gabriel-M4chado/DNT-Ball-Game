8f6bf4fa1e64eaa895e369b023e3c7f8cadef189
g4bx <gabriel.machado.js@gmail.com>
2023-06-06 22:25:11 -0300

    Criando tela End Game

---
+++ Game/src/view/TelaEndGame.java
@@ -0,0 +1,16 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class TelaEndGame extends JFrame{
+
+    public TelaEndGame() {
+        setTitle("Resultado");
+        setLayout(new BorderLayout());
+        setSize(400, 600);
+        setVisible(true);
+        setLocationRelativeTo(null);
+    }
+
+}
